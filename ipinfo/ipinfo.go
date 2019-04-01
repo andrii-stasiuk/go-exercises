2ff3bc90622bc1fa66dbc9063b9883a7db255757
Andrii Stasiuk <43143376+andrii-stasiuk@users.noreply.github.com>
2019-04-01 15:55:31 +0200

    Changed formatting and added JSON tags to the structure

--- ipinfo/ipinfo.go
+++ ipinfo/ipinfo.go
@@ -1,56 +1,57 @@
 package main
+
 import (
-    "fmt"
-    "flag"
-    "net/http"
-    "io/ioutil"
-    "encoding/json"
-    "net"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
 )
 
 type IPFields struct {
-    AS string
-    City string
-    Country string
-    CountryCode string
-    ISP string
-    Lat float64
-    Lon float64
-    Org string
-    Query string
-    Region string
-    RegionName string
-    Status string
-    Timezone string
-    Zip string
+	AS          string  `json:"as"`
+	City        string  `json:"city"`
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	ISP         string  `json:"isp"`
+	Lat         float64 `json:"lat"`
+	Lon         float64 `json:"lon"`
+	Org         string  `json:"org"`
+	Query       string  `json:"query"`
+	Region      string  `json:"region"`
+	RegionName  string  `json:"regionName"`
+	Status      string  `json:"status"`
+	Timezone    string  `json:"timezone"`
+	Zip         string  `json:"zip"`
 }
 
 func main() {
-    var ip string
-    var ipPtr = flag.String("ip", "81.190.40.214", "a string")
-    var geoPtr = flag.Bool("geo", false, "a bool")
-    flag.Parse()
+	var ip string
+	var ipPtr = flag.String("ip", "81.190.40.214", "a string")
+	var geoPtr = flag.Bool("geo", false, "a bool")
+	flag.Parse()
 
-    if net.ParseIP( * ipPtr) != nil {
-        ip = * ipPtr
-    } else {
-        ip = "81.190.40.214"
-    }
+	if net.ParseIP(*ipPtr) != nil {
+		ip = *ipPtr
+	} else {
+		ip = "81.190.40.214"
+	}
 
-    resp, err := http.Get("http://ip-api.com/json/" + ip)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
+	resp, err := http.Get("http://ip-api.com/json/" + ip)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 
-    var ipField IPFields
-    json.Unmarshal([] byte(body), & ipField)
+	var ipField IPFields
+	json.Unmarshal([]byte(body), &ipField)
 
-    if *geoPtr {
-        fmt.Printf("Loc: %.4f, %.4f", ipField.Lat, ipField.Lon)
-    } else {
-        fmt.Printf("IP address: %s\nOrganization: %s\nCity: %s\nRegion: %s\nCountry: %s\nLoc: %.4f, %.4f\nPostal: %s",
-            ipField.Query, ipField.Org, ipField.City, ipField.RegionName, ipField.Country, ipField.Lat, ipField.Lon, ipField.Zip)
-    }
+	if *geoPtr {
+		fmt.Printf("Loc: %.4f, %.4f", ipField.Lat, ipField.Lon)
+	} else {
+		fmt.Printf("IP address: %s\nOrganization: %s\nCity: %s\nRegion: %s\nCountry: %s\nLoc: %.4f, %.4f\nPostal: %s",
+			ipField.Query, ipField.Org, ipField.City, ipField.RegionName, ipField.Country, ipField.Lat, ipField.Lon, ipField.Zip)
+	}
 }
