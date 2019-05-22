2dfc889ffb8e1ac3c10c40cfc731bb293aee251b
Andrii Stasiuk <as@ges.sh>
2019-05-22 14:53:43 +0200

    Code optimization

--- pingpong/ping.go
+++ pingpong/ping.go
@@ -6,25 +6,19 @@ import (
 	"time"
 )
 
-func ping(ch chan string) {
+func pingpong(ch chan string) {
 	amt := time.Duration(100 + rand.Intn(900))
 	time.Sleep(time.Millisecond * amt)
-	ch <- "ping"
-}
-
-func pong(ch chan string) {
-	amt := time.Duration(100 + rand.Intn(900))
-	time.Sleep(time.Millisecond * amt)
-	ch <- "pong"
+	fmt.Println(<-ch)
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	ch := make(chan string)
 	for {
-		go ping(ch)
-		fmt.Println(<-ch)
-		go pong(ch)
-		fmt.Println(<-ch)
+		go pingpong(ch)
+		ch <- "ping"
+		go pingpong(ch)
+		ch <- "pong"
 	}
 }
