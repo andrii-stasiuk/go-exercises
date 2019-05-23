3650c26c0096e383e3f40c8ceb83e8c5a2b59b65
Andrii Stasiuk <43143376+andrii-stasiuk@users.noreply.github.com>
2019-05-23 20:50:06 +0200

    temp saving...

--- client-server/server.go
+++ client-server/server.go
@@ -22,10 +22,10 @@ func YourHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserCreator(w http.ResponseWriter, r *http.Request) {
-	mutex := new(sync.RWMutex)
-	innerDB := database{*mutex, map[uint64]User{}}
-	innerDB.Set()
-	w.Write([]byte("UserCreator\n"))
+	// mutex := new(sync.RWMutex)
+	// innerDB := Database{*mutex, map[uint64]User{}}
+	// innerDB.Set()
+	w.Write([]byte("Created user: " + NewDB().Set().Name + " " + NewDB().Set().Surname))
 }
 
 func UserDeleter(w http.ResponseWriter, r *http.Request) {
@@ -38,44 +38,62 @@ func UserGetter(w http.ResponseWriter, r *http.Request) {
 	//s := User{1, "2", "3", "4"}
 	vars := mux.Vars(r)
 	//w.Write([]byte("Get " + vars["id"]))
-
-	innerDB := new(database)
+	// mutex := new(sync.RWMutex)
+	// innerDB := Database{*mutex, map[uint64]User{}}
+	//innerDB := new(database)
 	//d.Get(12)
 	strtoint, _ := strconv.ParseUint(vars["id"], 10, 64)
 	//n, err := strconv.ParseInt(s, 10, 64)
 	// if err == nil {
 	// 	fmt.Printf("%d of type %T", n, n)
 	// }
-
-	w.Write([]byte(innerDB.Get(int(strtoint)).Name + innerDB.Get(int(strtoint)).Surname))
+	w.Write([]byte("Get user: " + NewDB().Get(int(strtoint)).Name + " " + NewDB().Get(int(strtoint)).Surname))
 	//w.Write([]byte("UserGetter\n"))
 }
 
-type database struct {
+type Database struct {
 	m     sync.RWMutex
 	users map[uint64]User
 }
 
-func (r database) Delete() {
+func (r *Database) Delete() {
+	delete(r.users, 2)
 }
 
-func (r database) Get(us int) (usr User) {
+func (r *Database) Get(us int) User {
 	if us == 1 {
 		return User{1, "Andrii", "Stasiuk", "as@ges.sh"}
 	} else {
-		return r.users[1]
+		return r.users[2]
 	}
 }
 
-func (r *database) Set() {
-	r.users[1] = User{1, "Andrii2", "Stasiuk2", "as@ges.sh"}
+func (r *Database) Set() User {
+	r.users[2] = User{2, "John", "Smith", "as@ges.sh"}
+	return r.users[2]
 	//r.users[id] := 12
 }
 
-//var innerDB database
+var innerDB *Database
+
+//var lock = &sync.Mutex{}
+func NewDB() *Database {
+	// lock.Lock()
+	// defer lock.Unlock()
+	if innerDB == nil {
+		mutex := new(sync.RWMutex)
+		innerDB = new(Database)
+		innerDB = &Database{*mutex, map[uint64]User{}}
+	}
+	return innerDB
+	// mutex := new(sync.RWMutex)
+	// return &Database{*mutex, map[uint64]User{}}
+}
 
 func main() {
 	fmt.Println("Apache Server Started")
+	// mutex := new(sync.RWMutex)
+	// innerDB := Database{*mutex, map[uint64]User{}}
 	//	innerDB := new(database)
 	//innerDB.Set()
 	r := mux.NewRouter()
