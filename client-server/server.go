944056b5588e9c614eb635f17a9caab982b52066
Andrii Stasiuk <as@ges.sh>
2019-05-24 09:36:36 +0200

    24.05.2019 9:35

--- client-server/server.go
+++ client-server/server.go
@@ -17,38 +17,30 @@ type User struct {
 	Email   string
 }
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("HTTP Error 503 (Service Unavailable)\n"))
 }
 
 func UserCreator(w http.ResponseWriter, r *http.Request) {
-	// mutex := new(sync.RWMutex)
-	// innerDB := Database{*mutex, map[uint64]User{}}
-	// innerDB.Set()
-	w.Write([]byte("Created user: " + NewDB().Set().Name + " " + NewDB().Set().Surname))
+	id := NewDB().Set().ID
+	usr := NewDB().Get(id)
+	w.Write([]byte("Created user: " + strconv.FormatUint(usr.ID, 10) + " " + usr.Name + " " + usr.Surname))
 }
 
 func UserDeleter(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	w.Write([]byte("Delete " + vars["id"]))
-	//w.Write([]byte("UserDeleter\n"))
+	strtoint, _ := strconv.ParseUint(vars["id"], 10, 64)
+	w.Write([]byte("Deleted: " + NewDB().Delete(strtoint)))
 }
 
 func UserGetter(w http.ResponseWriter, r *http.Request) {
-	//s := User{1, "2", "3", "4"}
 	vars := mux.Vars(r)
-	//w.Write([]byte("Get " + vars["id"]))
-	// mutex := new(sync.RWMutex)
-	// innerDB := Database{*mutex, map[uint64]User{}}
-	//innerDB := new(database)
-	//d.Get(12)
 	strtoint, _ := strconv.ParseUint(vars["id"], 10, 64)
 	//n, err := strconv.ParseInt(s, 10, 64)
 	// if err == nil {
 	// 	fmt.Printf("%d of type %T", n, n)
 	// }
-	w.Write([]byte("Get user: " + NewDB().Get(int(strtoint)).Name + " " + NewDB().Get(int(strtoint)).Surname))
-	//w.Write([]byte("UserGetter\n"))
+	w.Write([]byte("Get user: " + NewDB().Get(uint64(strtoint)).Name + " " + NewDB().Get(uint64(strtoint)).Surname))
 }
 
 type Database struct {
@@ -56,25 +48,29 @@ type Database struct {
 	users map[uint64]User
 }
 
-func (r *Database) Delete() {
-	delete(r.users, 2)
+func (r *Database) Delete(usr uint64) string {
+	dl := r.users[usr].Name + r.users[usr].Surname
+	delete(r.users, usr)
+	return dl
 }
 
-func (r *Database) Get(us int) User {
-	if us == 1 {
-		return User{1, "Andrii", "Stasiuk", "as@ges.sh"}
-	} else {
-		return r.users[2]
-	}
+func (r *Database) Get(usr uint64) User {
+	// if us == 1 {
+	// 	return User{1, "Andrii", "Stasiuk", "as@ges.sh"}
+	// } else {
+	return r.users[usr]
+	// }
 }
 
 func (r *Database) Set() User {
-	r.users[2] = User{2, "John", "Smith", "as@ges.sh"}
-	return r.users[2]
+	lastElem++
+	r.users[lastElem] = User{lastElem, "John" + strconv.FormatUint(lastElem, 10), "Smith", "as@ges.sh"}
+	return r.users[lastElem]
 	//r.users[id] := 12
 }
 
 var innerDB *Database
+var lastElem uint64
 
 //var lock = &sync.Mutex{}
 func NewDB() *Database {
@@ -86,23 +82,17 @@ func NewDB() *Database {
 		innerDB = &Database{*mutex, map[uint64]User{}}
 	}
 	return innerDB
-	// mutex := new(sync.RWMutex)
-	// return &Database{*mutex, map[uint64]User{}}
 }
 
 func main() {
 	fmt.Println("Apache Server Started")
-	// mutex := new(sync.RWMutex)
-	// innerDB := Database{*mutex, map[uint64]User{}}
-	//	innerDB := new(database)
-	//innerDB.Set()
 	r := mux.NewRouter()
 
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/", DefaultHandler)
 
-	r.HandleFunc("/users/", UserCreator).Methods("GET") // .Methods("POST")
-	r.HandleFunc("/users/{id:[0-9]+}/", UserDeleter).Methods("DELETE")
+	r.HandleFunc("/users/", UserCreator).Methods("GET")                 // .Methods("POST")
+	r.HandleFunc("/users/del/{id:[0-9]+}/", UserDeleter).Methods("GET") // .Methods("DELETE")
 	r.HandleFunc("/users/{id:[0-9]+}/", UserGetter).Methods("GET")
 
 	// Bind to a port and pass our router in
