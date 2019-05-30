b2816d7b8697fa2a72d53fa237c5fb8251f80e3a
Andrii Stasiuk <as@ges.sh>
2019-05-30 09:57:16 +0200

    Deleted unneeded comments in code

--- client-server/server.go
+++ client-server/server.go
@@ -17,7 +17,6 @@ func main() {
 	var newdbPtr = flag.Bool("create", false, "Create a new database")
 	var addrPtr = flag.String("addr", "127.0.0.1:8000", "Server IPv4 address")
 	var dbfilePtr = flag.String("file", "database.json", "Specify the name of the database file")
-	// #1.1 var delayPtr = flag.Int("delay", 60, "Delay in seconds between saving the database"
 	flag.Parse()
 
 	fmt.Println("API server starting...")
@@ -32,14 +31,7 @@ func main() {
 		fmt.Println("Using database: " + *dbfilePtr)
 	}
 
-	var hl *handlers.Handlers
-	hl = &handlers.Handlers{Database: *db}
-
-	// #1.2 This is done to reduce server load
-	// if *delayPtr < 5 {
-	// 	*delayPtr = 5
-	// }
-	// fmt.Printf("Autosave interval set to: %d seconds\n", *delayPtr)
+	hl := &handlers.Handlers{Database: *db}
 
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
@@ -51,7 +43,7 @@ func main() {
 	// Handlers reserved for testing purposes
 	r.HandleFunc("/users/save/", hl.UserSaver).Methods("GET")
 	r.HandleFunc("/users/load/", hl.UserLoader).Methods("GET")
-
+	w
 	srv := &http.Server{
 		Handler: r,
 		Addr:    *addrPtr,
@@ -61,8 +53,6 @@ func main() {
 	}
 
 	fmt.Println("API server started successfully on " + *addrPtr)
-
-	// #1.3. go db.RepeatSaving(*delayPtr)
 
 	log.Fatal(srv.ListenAndServe())
 }
